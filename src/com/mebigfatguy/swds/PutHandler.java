968192fbe48a2845ef0ef04e6840459a7fe84233
Dave Brosius <dbrosius@mebigfatguy.com>
2014-09-22 22:27:23 -0400

    stub in PUT handler

---
+++ src/com/mebigfatguy/swds/PutHandler.java
@@ -0,0 +1,14 @@
+package com.mebigfatguy.swds;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PutHandler implements HttpHandler {
+
+	@Override
+	public void handleRequest(HttpServletRequest req, HttpServletResponse resp, File rootDirectory) {
+	}
+
+}
