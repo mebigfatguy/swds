49812a9418cb3d42430361781a49aeebe90a10c5
Dave Brosius <dbrosius@mebigfatguy.com>
2014-11-10 23:47:38 -0500

    get MOVE working somewhat

--- src/com/mebigfatguy/swds/MoveHandler.java
+++ src/com/mebigfatguy/swds/MoveHandler.java
@@ -26,6 +26,27 @@ public class MoveHandler implements HttpHandler {
 
     @Override
     public void handleRequest(HttpServletRequest req, HttpServletResponse resp, File rootDirectory) throws ServletException {
+        
+        File srcFile = new File(rootDirectory, req.getPathInfo());
+        
+        String destUrl = req.getHeader("Destination");
+        String servletPath = req.getRequestURL().toString();
+        servletPath = servletPath.substring(0, servletPath.length() - req.getPathInfo().length());
+        File dstFile = new File(rootDirectory, destUrl.substring(servletPath.length() + 1));
+        
+        if (dstFile.exists()) {
+            dstFile.delete();
+        }
+        
+        if (!dstFile.getParentFile().exists()) {
+            resp.setStatus(HttpServletResponse.SC_CONFLICT);
+        }
+        
+        srcFile.renameTo(dstFile);
+        
+        resp.setStatus(HttpServletResponse.SC_CREATED);
+        resp.setHeader("Location", destUrl);
+        resp.setContentLength(0);
     }
 
 }
